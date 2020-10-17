23da38a37b91cae477b4320c981e40c49dc5191c
Evgeny Samoshin <samevgeny@gmail.com>
2020-10-18 01:35:28 +0400

    test

--- src/TodoList.js
+++ src/TodoList.js
@@ -4,10 +4,12 @@ import Todo from './Todo';
 
 const TodoList = (props) => {
     
+  const handleActivate = () => {};
+  
     return( 
       <List className='list-wrapper'>
         {props.tasks.map((todo, index) => (
-          <Todo content={todo} key={index} id={index} onDelete={props.onDelete} />
+          <Todo content={todo} key={index} id={index} onDelete={props.onDelete} onActive={handleActivate} />
           )     
         )}
       </List>
